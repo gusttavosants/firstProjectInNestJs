c00b2e0a95c19c6d33c3c37bfd7a9ef0e06c7029
gusttavosants <gustavoluizsantosmelo@gmail.com>
2025-11-10 21:50:33 -0300

    oi

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,22 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CustomersModule } from './customers/customers.module';
 import { EmployeeModule } from './employee/employee.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CustomersModule, EmployeeModule],
+  imports: [TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306, //padrao mysql
+    username: 'root',
+    password: '',
+    database: 'curso',
+    entities: [],
+    synchronize: false,  // importante! false em produção
+    logging: true,
+}),
+  
+  CustomersModule, EmployeeModule],
   controllers: [AppController],
   providers: [AppService],
 })
